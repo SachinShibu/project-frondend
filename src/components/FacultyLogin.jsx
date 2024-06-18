6a1016f8d39d24a72d867e752d9914dbcf3704c0
adithkrishnaor <adithkrishnaor@gmail.com>
2024-06-18 13:34:06 +0530

    modified ui and added api contents

--- src/components/FacultyLogin.jsx
+++ src/components/FacultyLogin.jsx
@@ -1,6 +1,36 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const FacultyLogin = () => {
+
+    const [data, setData] = useState(
+        {
+            "email": "",
+            "password": ""
+        }
+    )
+
+    const inputHandler = (event) => {
+        setData({ ...data, [event.target.name]: event.target.value })
+    }
+
+    const readValue = () => {
+        axios.post("", data)
+            .then(
+                (response) => {
+                    if (response.data.status == "success") {
+                        alert("Login Success")
+                    } else {
+                        alert("Login Failed")
+                    }
+                }
+            ).catch(
+                (error) => {
+                    alert(error.message)
+                })
+    }
+
     return (
         <div>
 
@@ -13,19 +43,28 @@ const FacultyLogin = () => {
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-                        <div className="row g-4">
-                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <label htmlFor="" className="form-label">Enter your Email</label>
-                                <input type="text" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <label htmlFor="" className="form-label">Enter your Password</label>
-                                <input type="password" name="" id="" className="form-control" />
-                            </div>
-                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                                <button className="btn btn-success">Login</button>
+                        <div class="card w-50 mx-auto border-rounded text-bg-light border-info">
+                            <div class="card-body">
+                                <div className="row g-4">
+                                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                        <label htmlFor="" className="form-label">Enter your Email</label>
+                                        <input type="text" className="form-control"name='email' value={data.email} onChange={inputHandler}/>
+                                    </div>
+                                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                        <label htmlFor="" className="form-label">Enter your Password</label>
+                                        <input type="password" name="password" id="" className="form-control" value={data.password} onChange={inputHandler}/>
+                                    </div>
+                                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                        <center>
+                                            <button className="btn btn-success" onClick={readValue}>Login</button>
+                                            <br /><br /><Link to="/"> Back to home page</Link>
+                                        </center>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
+
                     </div>
                 </div>
             </div>
